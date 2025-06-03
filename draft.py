7249aa9980fe1f811f2af6b23771b683458d606c
David <davicolo717@gmail.com>
2025-06-03 12:39:35 -0400

    The_Chris

--- draft.py
+++ draft.py
@@ -1,5 +1,13 @@
-print("Hello world")
 
-print("Hello world")
+opt = int(input("Insert the option: "))
 
-print("Hello world")
+
+while True:
+    if opt == 1:
+        print("The Chris is gay")
+    elif opt == 2:
+        print("The Chris is not gay")
+    elif opt == 3:
+        print("The Chris is gay")
+    elif opt == 4:
+        print("The Chris is not gay")
