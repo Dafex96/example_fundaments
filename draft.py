6711cbe9ef762755b61c7d415213576a824fcf25
David <davicolo717@gmail.com>
2025-06-12 13:44:05 -0400

    ñ

--- draft.py
+++ draft.py
@@ -1,5 +1,8 @@
 league = {"01":["Colo-Colo"], "02":["Universidad de Chile"],
 "03":["Universidad Católica"], "04":["Union Española"]}
 
+for key, teams in league.items():
+    print(key, teams[0])
+
 for i in league:
-    print(i)
+    print(league[i])
