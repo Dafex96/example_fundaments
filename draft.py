4a015677c1b027c92dbaa25e827856b4334026e3
David <davicolo717@gmail.com>
2025-06-12 14:14:41 -0400

    Teams Shirts

--- draft.py
+++
@@ -1,8 +0,0 @@
-league = {"01":["Colo-Colo"], "02":["Universidad de Chile"],
-"03":["Universidad Católica"], "04":["Union Española"], "05":["Magallanes"]}
-
-for key, teams in league.items():
-    print(key, teams[0])
-
-for i in league:
-    print(league[i])
---
+++ football_shirts.py
@@ -0,0 +1,27 @@
+# league = {"01":["Colo-Colo"], "02":["Universidad de Chile"],
+# "03":["Universidad Católica"], "04":["Union Española"], "05":["Magallanes"]}
+
+teams = ["Colo-Colo","Universidad de Chile","Universidad Católica","Magallanes","Curicó Unido"]
+
+def menu():
+    print("--Football Teams Shits--")
+    print("1) Colo-Colo")
+    print("2) Universidad de Chile")
+    print("3) Universidad Católica")
+    print("4) Magallanes")
+    print("5) Curicó Unido")
+    print("6) Quit")
+
+while True:
+    try:
+        menu()
+        user = int(input("Choose your team: "))
+        break
+    except ValueError:
+        print("You need to enter a valid option, try again.")
+
+while True:
+    if user == 1:
+        h_a = input("Do you want the home or away version?: ")
+        quant = int(input("How many shirts do you want?: "))
+        print(f"Purchase successful! You bought {quant} {h_a} version shirts")
